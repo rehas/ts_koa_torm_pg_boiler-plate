e4df8e037792e48c79b4dd83ef3edee1baa2f80d
RehaS <beratson@gmail.com>
2018-10-29 19:13:05 +0100

    Koa Server init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import {Server} from 'http';
+import * as Koa from 'koa';
+
+const app = new Koa();
+const server = new Server(app.callback());
+const port = process.env.PORT || 4000;
+
 console.log("Hello World, boiler plate code init!");
+
+server.listen(port, x=> console.log("Hola, suave mente!" + x  ));
+console.log(`Listening on port ${port}`);
