b641c6ec8fe5b91c2c16cef1aef0f9ff52510bd1
RehaS <beratson@gmail.com>
2018-10-30 16:58:19 +0100

    postgres included in project, db setup via code, auto table generation enabled

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {useKoaServer} from 'routing-controllers';
 import {Server} from 'http';
 import * as Koa from 'koa';
 import UserController from './users/controller';
+import setupDb from './db'
 
 const app = new Koa();
 const server = new Server(app.callback());
@@ -17,5 +18,10 @@ useKoaServer(app, {
 
 console.log("Hello World, boiler plate code init!");
 
-server.listen(port, x=> console.log("Hola, suave mente!" + x  ));
-console.log(`Listening on port ${port}`);
+setupDb()
+  .then(_=> {
+    server.listen(port, x=> console.log("Hola, suave mente!" + x  ));
+    console.log(`Listening on port ${port}`);
+  })
+  .catch(err=> console.log(err))
+
--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,7 +1,8 @@
-import {BaseEntity, Entity, Column} from 'typeorm';
+import {BaseEntity, Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity()
 export default class User extends BaseEntity{
+  @PrimaryGeneratedColumn()
   id: number
 
   @Column('text')
